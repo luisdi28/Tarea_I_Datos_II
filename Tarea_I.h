5f8be0e517113a0e3de4fd04b11a862f1472bcc4
luisdi28 <luisdi.28@estudiantec.cr>
2023-03-02 22:55:52 -0600

    commit #1

---
+++ Tarea_I.h
@@ -0,0 +1,14 @@
+//
+// Created by luis on 02/03/23.
+//
+
+#ifndef TAREA_I_DATOS_II_TAREA_I_H
+#define TAREA_I_DATOS_II_TAREA_I_H
+
+
+class Tarea_I {
+
+};
+
+
+#endif //TAREA_I_DATOS_II_TAREA_I_H
