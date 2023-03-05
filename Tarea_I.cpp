ff515b3b6982509b8d703cc7277ab16e275cc5da
luisdi28 <luisdi.28@estudiantec.cr>
2023-03-05 16:30:33 -0600

    Commit #4
    Se creo el método print para la clase List

--- Tarea_I.cpp
+++ Tarea_I.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 
+class Collector;
+
 class Node {
 public:
     int value;
@@ -8,16 +10,30 @@ public:
 
     Node(int val, Node* nxt=nullptr) : value(val), next(nxt) {}
 
+    // sobrecarga de new y delete
+    void* operator new(size_t size);
+    void operator delete(void* ptr);
+
+    friend class Collector;
 };
 
 class List {
 public:
-    Node *head;
+    Node* head;
 
     List() : head(nullptr) {}
 
     void insert(int val) {
-        Node *new_node = new Node(val, head);
+        Node* new_node = new Node(val, head);
         head = new_node;
+    }
+
+    void print() {
+        Node* curr = head;
+        while (curr != nullptr) {
+            std::cout << curr->value << " ";
+            curr = curr->next;
+        }
+        std::cout << std::endl;
     }
 };
